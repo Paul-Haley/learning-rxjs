b52c1e8824bc94a2cd73e7ae52f4827b310a3282
Paul Haley <paul.haley@citrusad.com>
2022-07-26 16:43:01 +1000

    first observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {of} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learning-rxjs';
+
+  constructor() {
+    // taken from: https://rxjs-dev.firebaseapp.com/api/index/class/Observable#create
+    const sumObserver = {
+      sum: 0,
+      next(value: number) {
+        console.log('Adding: ' + value);
+        this.sum = this.sum + value;
+      },
+      error() {
+        // We actually could just remove this method,
+        // since we do not really care about errors right now.
+      },
+      complete() {
+        console.log('Sum equals: ' + this.sum);
+      }
+    };
+
+    of(1, 2, 3) // Synchronously emits 1, 2, 3 and then completes.
+      .subscribe(sumObserver);
+  }
 }
