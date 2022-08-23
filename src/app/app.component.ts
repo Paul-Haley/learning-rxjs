56c9f4769900c65173fa3b00a7f14cad1d800a92
Paul Haley <paul.haley@citrusad.com>
2022-08-23 16:39:39 +1000

    subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Observable, of} from "rxjs";
+import {Observable, of, Subject} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -8,24 +8,18 @@ import {Observable, of} from "rxjs";
 })
 export class AppComponent {
   title = 'learning-rxjs';
-  sumOb: Observable<number> = of(1, 2, 3);
+  private mySubject: Subject<number> = new Subject();
 
   ngOnInit() {
-    // adapted from: https://rxjs-dev.firebaseapp.com/api/index/class/Observable#create
-     const sumObserver = {
-      sum: 0,
-      next(value: number) {
-        console.log('Adding: ' + value);
-        this.sum = this.sum + value;
-      },
-      error() {
-        // We actually could just remove this method,
-        // since we do not really care about errors right now.
-      },
-      complete() {
-        console.log('Sum equals: ' + this.sum);
-      }
-    };
-    this.sumOb.subscribe(sumObserver);
+    this.mySubject.subscribe(x => console.log('first subscribe', x));
+    this.mySubject.next(1);
+    this.mySubject.next(2);
+    this.mySubject.subscribe(x => console.log('second subscribe', x));
+    this.mySubject.next(3);
+
+  }
+
+  ngOnDestroy() {
+    this.mySubject.unsubscribe();
   }
 }
