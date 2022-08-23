6fa8b1a7fcebe955d97c0b407724a4d46255495b
Paul Haley <paul.haley@citrusad.com>
2022-08-23 16:50:39 +1000

    BS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Observable, of, Subject} from "rxjs";
+import {BehaviorSubject, Observable, of, Subject} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +8,7 @@ import {Observable, of, Subject} from "rxjs";
 })
 export class AppComponent {
   title = 'learning-rxjs';
-  private mySubject: Subject<number> = new Subject();
+  private mySubject: Subject<number> = new BehaviorSubject(200);
 
   ngOnInit() {
     this.mySubject.subscribe(x => console.log('first subscribe', x));
